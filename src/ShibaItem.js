48b95e5cdf3b833c2c6074ed813500c0004c05b1
Mari Anuashvili <m.anuashvili.com>
2023-09-25 10:45:45 -0400

    refactored ShibaItem to an arrow fn component

--- src/ShibaItem.js
+++ src/ShibaItem.js
@@ -1,16 +1,14 @@
 import React from 'react';
 // import { shibas } from './shibas.js'
 
-class ShibaItem extends React.Component { // class component
-  render() {
-    console.log("PROPS OF SHIBA ITEM:", this.props)
-    return(
-      <div>
-        <li>Name: {this.props.name}</li>
-        <li>Gender: {this.props.gender}</li>
-      </div>
-    ) 
-  }
+let ShibaItem = (props) => {
+    // console.log("PROPS OF SHIBA ITEM:", props)
+  return(
+    <div>
+      <li>Name: {props.name}</li>
+      <li>Gender: {props.gender}</li>
+    </div>
+  ) 
 }
 
 export default ShibaItem; 
