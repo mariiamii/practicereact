d813425088b0e36725d99eff7e03a819d6465cbb
Mari Anuashvili <m.anuashvili@gmail.com>
2023-09-25 12:19:10 -0400

    small change

--- src/ShibaContainer.js
+++ src/ShibaContainer.js
@@ -2,33 +2,33 @@ import React, { useState} from 'react';
 import ShibaItem from './ShibaItem';
 
 let ShibaContainer = (props) => { // arrow fn component
-    // console.log("THIS IS THE PROPS OF SHIBA CONTAINER:", props)
-    let [changeText, setChangeText] = useState(true);
+  // console.log("THIS IS THE PROPS OF SHIBA CONTAINER:", props)
+  let [changeText, setChangeText] = useState(true);
 
-    const handleChange = () => {
-        return setChangeText(!changeText);
-    }
+  const handleChange = () => {
+    return setChangeText(!changeText);
+  }
 
-    return (
-        <main>
-            <p>{props.title}</p>
-            <button onClick={() => handleChange()}>Click me</button>
-            <div>
-                {changeText ? (
-                    <>
-                        <ShibaItem name="Charlie" gender="male"/>
-                        <ShibaItem name="Felix" gender="male"/> 
-                    </>
-                    ) : (
-                    <>
-                        <ShibaItem name="Lilo" gender="female"/>
-                        <ShibaItem name="Eevee" gender="female"/>
-                    </>
-                    )
-                }
-            </div>
-        </main>
-    )
+  return (
+    <main>
+      <p>{props.title}</p>
+      <button onClick={() => handleChange()}>Click me</button>
+      <div>
+        {changeText ? (
+          <>
+            <ShibaItem name="Charlie" gender="male"/>
+            <ShibaItem name="Felix" gender="male"/> 
+          </>
+          ) : (
+          <>
+            <ShibaItem name="Lilo" gender="female"/>
+            <ShibaItem name="Eevee" gender="female"/>
+          </>
+          )
+        }
+      </div>
+    </main>
+  )
 } 
-
+// testing
 export default ShibaContainer;
