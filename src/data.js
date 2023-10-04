d400c549c657ef14712a17a246aac37b57708297
Mari Anuashvili <m.anuashvili@gmail.com>
2023-10-04 14:55:31 -0400

    updated data.js

---
+++ src/data.js
@@ -0,0 +1,87 @@
+const data = [
+    {
+        id: 1,
+        img: "https://images.saymedia-content.com/.image/c_limit%2Ccs_srgb%2Cq_auto:eco%2Cw_620/MTk2MzA1MDEwMTg5OTM1OTIw/strong-and-feminine-shiba-inu-names.webp",
+        gender: "Female",
+        name: "Lilo",
+    },
+
+    {
+        id: 2,
+        img: "https://i.pinimg.com/1200x/67/5a/8e/675a8e274eaea3ba80200c0979386eab.jpg",
+        gender: "Female",
+        name: "Evie",
+    },
+
+    {
+        id: 3,
+        img: "https://www.akc.org/wp-content/uploads/2017/11/Shiba-Inu-puppy-standing-outdoors.jpg",
+        gender: "Male",
+        name: "Felix",
+    },
+
+    {
+        id: 4,
+        img: "https://everylittlename.com/wp-content/uploads/2022/07/shiba-inu-standing-grass.jpg",
+        gender: "Male",
+        name: "Jack",
+    },
+
+    {
+        id: 5,
+        img: "https://www.bil-jac.com/media/kkobwqpy/shiba-inu-1047146596.jpg",
+        gender: "Male",
+        name: "Jake",
+    },
+
+    {
+        id: 6,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQWiErpbGnDJ_gphlVwlJ8RPmQFGTx8WAp7Q&usqp=CAU",
+        gender: "Female",
+        name: "Emmy",
+    },
+
+    {
+        id: 7,
+        img: "https://cdn.pixabay.com/photo/2018/10/04/19/46/dog-3724261_640.jpg",
+        gender: "Female",
+        name: "Lucy",
+    },
+
+    {
+        id: 8,
+        img: "https://media.istockphoto.com/id/1283508081/photo/gorgeous-and-happy-red-shiba-inu-dog-sitting-in-the-violet-flowers-field-phacelia-blossoms.jpg?s=612x612&w=0&k=20&c=UUiorgBY5PVFZGc-GbPIi37v7F0vKabeiQJ4VoPE0iM=",
+        gender: "Female",
+        name: "Lichee",
+    },
+
+    {
+        id: 9,
+        img: "https://media.istockphoto.com/id/1328030222/photo/cute-ginger-dog-of-shiba-inu-breed-sitting-wrapped-in-warm-blanket-on-stone-japanese-lantern.webp?b=1&s=170667a&w=0&k=20&c=dDkfXRCJY4uKAnbYl7TPyH71RUs9e-7dBwWd7GN-QiQ=",
+        gender: "Female",
+        name: "Feebee",
+    },
+
+    {
+        id: 10,
+        img: "https://i.pinimg.com/736x/4f/7e/f5/4f7ef5f81ca49f259e9cbfd3852907ef.jpg",
+        gender: "Male",
+        name: "Evo",
+    },
+
+    {
+        id: 11,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0NvHXNNz9YKT5vzS44JfIrsbhRN79evku3GZVF2ndnh9wdHQBH18x8XKdfV4rw3CVHiY&usqp=CAU",
+        gender: "Male",
+        name: "Ricky",
+    },
+
+    {
+        id: 12,
+        img: "https://mymodernmet.com/wp/wp-content/uploads/2020/09/smilling-shiba-inu-10.jpg",
+        gender: "Male",
+        name: "Mickey",
+    }
+];
+
+export default data
