aab01706f517fdbce9c14ccb7918593c08c14cb4
Mari Anuashvili <m.anuashvili@gmail.com>
2023-10-10 12:06:34 -0400

    back to shibas

--- src/ShibaContainer.js
+++ src/ShibaContainer.js
@@ -1,79 +1,61 @@
 import { React, useState } from 'react'; // declares a state variable
 import ShibaItem from './ShibaItem'
 
-// Exercise practice:
-let ShibaContainer= (props) => {
-  // Syntax: const [state, setState] = useState(initialState);
-  const [nameInput, setNameInput] = useState("");
-  const [difficultyInput, setDifficultyInput] = useState("");
-  const [equipmentInput, setEquipmentInput] = useState("");
+let ShibaContainer = (props) => { // arrow fn component
+  const [inputText, setInputText] = useState("");
 
-  let inputHandler = (e) => { // event.target
+  let inputHandler = (e) => {
     let lowerCase = e.target.value.toLowerCase();
-    setNameInput(lowerCase);
-    setDifficultyInput(lowerCase);
-    setEquipmentInput(lowerCase);
+    setInputText(lowerCase);
   }
   
   return (
     <div>
       <label>
-        Search for exercise by name: 
+        Search for a Shiba by name: 
         <input 
           name='myInput' 
           onChange={inputHandler}
         />
       </label>
-      <ShibaItem 
-        nameInput={nameInput}
-        difficultyInput={difficultyInput}
-        equipmentInput={equipmentInput}
-      />
+      <ShibaItem input={inputText}/>
     </div>
   )
-}
+} 
 
-export default ShibaContainer
-
-
+export default ShibaContainer;
 
 
+// Exercise practice:
+// let ShibaContainer= (props) => {
+//   // Syntax: const [state, setState] = useState(initialState);
+//   const [nameInput, setNameInput] = useState("");
+//   const [difficultyInput, setDifficultyInput] = useState("");
+//   const [equipmentInput, setEquipmentInput] = useState("");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let ShibaContainer = (props) => { // arrow fn component
-//   const [inputText, setInputText] = useState("");
-
-//   let inputHandler = (e) => {
+//   let inputHandler = (e) => { // event.target
 //     let lowerCase = e.target.value.toLowerCase();
-//     setInputText(lowerCase);
+//     setNameInput(lowerCase);
+//     setDifficultyInput(lowerCase);
+//     setEquipmentInput(lowerCase);
 //   }
   
 //   return (
 //     <div>
 //       <label>
-//         Search for a Shiba by name: 
+//         Search for exercise by name: 
 //         <input 
 //           name='myInput' 
 //           onChange={inputHandler}
 //         />
 //       </label>
-//       <ShibaItem input={inputText}/>
+//       <ShibaItem 
+//         nameInput={nameInput}
+//         difficultyInput={difficultyInput}
+//         equipmentInput={equipmentInput}
+//       />
 //     </div>
 //   )
-// } 
+// }
 
-// export default ShibaContainer;
+// export default ShibaContainer
--- src/ShibaItem.js
+++ src/ShibaItem.js
@@ -1,98 +1,39 @@
 import React from 'react';
 import data from './data.js'
 
-// Exercise practice:
+// Shibas:
 let ShibaItem = (props) => {
-  let filteredData = data.filter((exercisePOJO) => {
-    let name = exercisePOJO.name.toLowerCase().includes(props.nameInput);
-    let difficulty = exercisePOJO.difficulty.toLowerCase().includes(props.difficultyInput);
-    let equipment = exercisePOJO.equipment.toLowerCase().includes(props.equipmentInput);
+  // let {name, image} = props // destructuring
 
-    if (props.nameInput === '' || props.difficultyInput === '' || props.equipmentInput === '') { 
-      return exercisePOJO;
-    } else if(name) {
-      return name;
-    } else if(difficulty) {
-      return difficulty;
-    } else{
-      return equipment;
+  // let handleClick = () => { // callback fn
+  //   console.log("❤️ for " + props.name)
+  // }
+
+  // console.log(props.input)
+  let filteredData = data.filter((shibaItem) => {
+    if (props.input === '') {
+      return shibaItem
+    } else {
+      return shibaItem.name.toLowerCase().includes(props.input)
     }
-  });
+  })
 
   return (
     <div className='shiba-container'>
-      {filteredData.map((exercise) => (
+      {filteredData.map((shiba) => (
         <ul className='shiba-ul'>
-            <li>
-              <p>Name: {exercise.name}</p>
-              <p>Difficulty: {exercise.difficulty}</p>
-              <p>Equipment: {exercise.equipment}</p>
-            </li>
+          <li>
+            <img key={shiba.id} src={shiba.img} alt="shiba" className="shiba-img"/>
+            <p>Name: {shiba.name}</p>
+          </li>
         </ul>
       ))}
     </div>
   ) 
 }
-  
+    
 export default ShibaItem; 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Shibas:
-// let ShibaItem = (props) => {
-//   // let {name, image} = props // destructuring
-
-//   // let handleClick = () => { // callback fn
-//   //   console.log("❤️ for " + props.name)
-//   // }
-  
-//   // console.log(props.input)
-//   let filteredData = data.filter((shibaItem) => {
-//     if (props.input === '') {
-//       return shibaItem
-//     } else {
-//       return shibaItem.name.toLowerCase().includes(props.input)
-//     }
-//   })
-
-//   return (
-//     <div className='shiba-container'>
-//       {filteredData.map((shiba) => (
-//         <ul className='shiba-ul'>
-//             <li>
-//               <img key={shiba.id} src={shiba.img} alt="shiba" className="shiba-img"/>
-//               <p>Name: {shiba.name}</p>
-//             </li>
-//         </ul>
-//       ))}
-//     </div>
-//   ) 
-// }
-  
-// export default ShibaItem; 
-  
-// <div>
-//   <img src={image} alt="shiba" className="shiba-img"/>
-//   <p>Name: {name}</p>
-//   {/* <button onClick={handleClick}>❤️</button> */}
-// </div>
-
+    
 // Jokes practice:
 // let ShibaItem = (props) => {
 //   let filteredData = data.filter((jokePOJO) => {
@@ -107,11 +48,46 @@ export default ShibaItem;
 //     <div className='shiba-container'>
 //       {filteredData.map((jokes) => (
 //         <ul className='shiba-ul'>
-//             <li>
-//               <p>{jokes.joke}</p>
-//             </li>
+//           <li>
+//             <p>{jokes.joke}</p>
+//           </li>
 //         </ul>
 //       ))}
+//     </div>
+//   ) 
+// }
+  
+// export default ShibaItem; 
+
+// Exercise practice:
+// let ShibaItem = (props) => {
+//   let filteredData = data.filter((exercisePOJO) => {
+//     let name = exercisePOJO.name.toLowerCase().includes(props.nameInput);
+//     let difficulty = exercisePOJO.difficulty.toLowerCase().includes(props.difficultyInput);
+//     let equipment = exercisePOJO.equipment.toLowerCase().includes(props.equipmentInput);
+
+//     if (props.nameInput === '' || props.difficultyInput === '' || props.equipmentInput === '') { 
+//       return exercisePOJO;
+//     } else if(name) {
+//       return name;
+//     } else if(difficulty) {
+//       return difficulty;
+//     } else{
+//       return equipment;
+//     }
+//   });
+
+//   return (
+//     <div className='shiba-container'>
+//       {filteredData.map((exercise) => (
+//         <ul className='shiba-ul'>
+//             <li>
+//               <p>Name: {exercise.name}</p>
+//               <p>Difficulty: {exercise.difficulty}</p>
+//               <p>Equipment: {exercise.equipment}</p>
+//             </li>
+//         </ul>
+//       ))}      
 //     </div>
 //   ) 
 // }
